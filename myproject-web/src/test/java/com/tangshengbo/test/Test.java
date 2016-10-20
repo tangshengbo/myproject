c64177566cc4fdcb906d9f8013854011936d93b2
tangshengbo <867850520tangbo@sian.com>
2016-10-20 21:53:32 +0800

    修改测试类

--- myproject-web/src/test/java/com/tangshengbo/test/Test.java
+++ myproject-web/src/test/java/com/tangshengbo/test/Test.java
@@ -1,6 +1,9 @@
 package com.tangshengbo.test;
 
+import com.tangshengbo.model.Account;
+import com.tangshengbo.service.AccountService;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,9 +13,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath*:/resources/*.xml")
 public class Test {
+    @Autowired
+    private AccountService accountService;
 
     @org.junit.Test
     public void test(){
+        Account account = new Account();
+        account.setMoney(232.3);
+        account.setName("thread");
+        accountService.addAccount(account);
         System.out.println("...............");
     }
 
