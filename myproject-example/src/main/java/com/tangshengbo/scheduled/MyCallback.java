ec534500b9eb437b4a5294c906511fa7f023217e
Tang <867850520tangbo@sina.com>
2017-06-30 18:17:28 +0800

    add callback code

---
+++ myproject-example/src/main/java/com/tangshengbo/scheduled/MyCallback.java
@@ -0,0 +1,11 @@
+package com.tangshengbo.scheduled;
+
+import com.tangshengbo.jms.activemq.JmsTemplate;
+
+/**
+ * Created by Tang on 2017/6/30.
+ */
+public interface MyCallback<T> {
+
+    T doExecutor(JmsTemplate jmsTemplate) throws Exception;
+}
