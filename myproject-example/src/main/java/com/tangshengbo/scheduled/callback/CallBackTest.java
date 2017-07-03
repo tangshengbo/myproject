025f9b3d668878d13ea5b0bee2a0b30eb87e71b6
tangshengbo <867850520tangbo@sian.com>
2017-07-03 22:23:46 +0800

    update callback code

--- myproject-example/src/main/java/com/tangshengbo/scheduled/callback/CallBackTest.java
+++ myproject-example/src/main/java/com/tangshengbo/scheduled/callback/CallBackTest.java
@@ -1,12 +1,21 @@
 package com.tangshengbo.scheduled.callback;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by TangShengBo on 2017-07-03.
  */
 public class CallBackTest {
 
     public static void main(String[] args) {
-        Teacher teacher = new Teacher();
-        teacher.publish(new Jack("Jack"));
+        Teacher teacher = new Teacher(new Jack("Jack"));
+        Thread thread = new Thread(teacher);
+        thread.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("continue..............");
     }
 }
--- myproject-example/src/main/java/com/tangshengbo/scheduled/callback/Teacher.java
+++ myproject-example/src/main/java/com/tangshengbo/scheduled/callback/Teacher.java
@@ -3,11 +3,22 @@ package com.tangshengbo.scheduled.callback;
 /**
  * Created by TangShengBo on 2017-07-03.
  */
-public class Teacher implements  CallBack {
+public class Teacher implements  CallBack, Runnable {
 
-    public void publish(Student student) {
+    private Student student;
+
+    public Teacher(Student student) {
+        this.student = student;
+    }
+
+    private void publish() {
         System.out.println("老师发布作业................");
         student.doWork(this);
+    }
+
+    @Override
+    public void run() {
+       this.publish();
     }
 
     @Override
