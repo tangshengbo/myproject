7da3cfa8e82ebb280b97b63e7d2353cbd65704ae
Tang <867850520tangbo@sina.com>
2017-07-14 17:41:58 +0800

    update nio code

--- myproject-example/src/main/java/com/tangshengbo/io/nio/NIOClient.java
+++ myproject-example/src/main/java/com/tangshengbo/io/nio/NIOClient.java
@@ -16,11 +16,12 @@ public class NIOClient {
 
     /**
      * 获得一个Socket通道，并对该通道做一些初始化的工作
-     * @param ip 连接的服务器的ip
-     * @param port  连接的服务器的端口号
+     *
+     * @param ip   连接的服务器的ip
+     * @param port 连接的服务器的端口号
      * @throws IOException
      */
-    public void initClient(String ip,int port) throws IOException {
+    public void initClient(String ip, int port) throws IOException {
         // 获得一个Socket通道
         SocketChannel channel = SocketChannel.open();
         // 设置通道为非阻塞
@@ -30,13 +31,14 @@ public class NIOClient {
 
         // 客户端连接服务器,其实方法执行并没有实现连接，需要在listen（）方法中调
         //用channel.finishConnect();才能完成连接
-        channel.connect(new InetSocketAddress(ip,port));
+        channel.connect(new InetSocketAddress(ip, port));
         //将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_CONNECT事件。
         channel.register(selector, SelectionKey.OP_CONNECT);
     }
 
     /**
      * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，则进行处理
+     *
      * @throws IOException
      */
     @SuppressWarnings("unchecked")
@@ -55,7 +57,7 @@ public class NIOClient {
                     SocketChannel channel = (SocketChannel) key
                             .channel();
                     // 如果正在连接，则完成连接
-                    if(channel.isConnectionPending()){
+                    if (channel.isConnectionPending()) {
                         channel.finishConnect();
 
                     }
@@ -63,7 +65,7 @@ public class NIOClient {
                     channel.configureBlocking(false);
 
                     //在这里可以给服务端发送信息哦
-                    channel.write(ByteBuffer.wrap(new String("向服务端发送了一条信息").getBytes()));
+                    channel.write(ByteBuffer.wrap(new String("向服务端发送了一条信息").getBytes("UTF-8")));
                     //在和服务端连接成功之后，为了可以接收到服务端的信息，需要给通道设置读的权限。
                     channel.register(this.selector, SelectionKey.OP_READ);
 
@@ -76,22 +78,25 @@ public class NIOClient {
 
         }
     }
+
     /**
      * 处理读取服务端发来的信息 的事件
+     *
      * @param key
      * @throws IOException
      */
-    public void read(SelectionKey key) throws IOException{
+    public void read(SelectionKey key) throws IOException {
         //和服务端的read方法一样
     }
 
     public static void main(String[] args) {
 //        writeFile();
 //        readFile();
-        copyFile();
+//        copyFile();
+        transfer();
 //        NIOClient client = new NIOClient();
 //        try {
-//            client.initClient("localhost",8000);
+//            client.initClient("localhost", 8000);
 //            client.listen();
 //        } catch (IOException e) {
 //            e.printStackTrace();
@@ -120,7 +125,7 @@ public class NIOClient {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 fis.close();
             } catch (IOException e) {
@@ -149,7 +154,7 @@ public class NIOClient {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 fos.close();
             } catch (IOException e) {
@@ -158,7 +163,7 @@ public class NIOClient {
         }
     }
 
-    private static void copyFile(){
+    private static void copyFile() {
         FileInputStream fis = null;
         FileOutputStream fos = null;
         FileChannel fcIn;
@@ -190,7 +195,7 @@ public class NIOClient {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 fos.close();
                 fis.close();
@@ -198,6 +203,34 @@ public class NIOClient {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private static void transfer() {
+        RandomAccessFile fromFile = null;
+        RandomAccessFile toFile = null;
+        try {
+            fromFile = new RandomAccessFile("E:/fromFile.txt", "rw");
+            toFile = new RandomAccessFile("E:/toFile.txt", "rw");
+
+            FileChannel toChannel = toFile.getChannel();
+            FileChannel fromChannel = fromFile.getChannel();
+            long position = 0;
+            long count = fromChannel.size();
+
+            fromChannel.transferTo(position, count, toChannel);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                toFile.close();
+                fromFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
