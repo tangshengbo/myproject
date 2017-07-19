79be15fa311405b25b4b96b686b9f02a90a6b6e1
tangshengbo <867850520tangbo@sian.com>
2017-07-19 22:31:15 +0800

    add apache http client code

--- myproject-example/src/main/java/com/tangshengbo/tutorial/http/HttpClient.java
+++ myproject-example/src/main/java/com/tangshengbo/tutorial/http/HttpClient.java
@@ -1,5 +1,14 @@
 package com.tangshengbo.tutorial.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,7 +22,7 @@ public class HttpClient {
 
     public static void main(String[] args) {
         try {
-            URL url = new URL("http://localhost:8090/user/pagehelper/50000/10000");
+            URL url = new URL("http://localhost:8083/producer/currentDateTime?token=10");
             String line;
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             while ((line = br.readLine()) != null) {
@@ -23,6 +32,50 @@ public class HttpClient {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        get();
+
+    }
+    /**
+     * 发送 get请求
+     */
+    public static void get() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            // 创建httpget.
+            HttpGet httpget = new HttpGet("http://localhost:8083/producer/currentDateTime?token=10");
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                System.out.println("--------------------------------------");
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity));
+                }
+                System.out.println("------------------------------------");
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
