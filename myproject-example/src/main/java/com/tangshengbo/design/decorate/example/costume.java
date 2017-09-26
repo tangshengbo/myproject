3aaf3bc7928bb2efc4af95a9a5e296d824f1cf46
Tangshengbo <867850520tangbo@sina.com>
2017-09-26 14:31:27 +0800

    add auto closeable code

--- myproject-example/src/main/java/com/tangshengbo/design/decorate/example/costume.java
+++ myproject-example/src/main/java/com/tangshengbo/design/decorate/example/costume.java
@@ -3,7 +3,7 @@ package com.tangshengbo.design.decorate.example;
 /**
  * Created by TangShengBo on 2017-09-24.
  */
-public class Costume extends Person {
+public abstract class Costume extends Person {
 
     private Person component;
 
