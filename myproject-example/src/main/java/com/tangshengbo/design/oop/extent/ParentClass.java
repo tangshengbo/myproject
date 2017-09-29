6ea856052e3a263f446771e646131a85208eac82
Tangshengbo <867850520tangbo@sina.com>
2017-09-29 18:47:36 +0800

    add extend code

---
+++ myproject-example/src/main/java/com/tangshengbo/design/oop/extent/ParentClass.java
@@ -0,0 +1,9 @@
+package com.tangshengbo.design.oop.extent;
+
+/**
+ * Created by Tangshengbo on 2017/9/29.
+ */
+public class ParentClass {
+
+    public int i = 100;
+}
---
+++ myproject-example/src/main/java/com/tangshengbo/design/oop/extent/SubClass.java
@@ -0,0 +1,12 @@
+package com.tangshengbo.design.oop.extent;
+
+/**
+ * Created by Tangshengbo on 2017/9/29.
+ */
+public class SubClass extends ParentClass {
+
+    public void init() {
+//        this.i = 100;
+    }
+
+}
