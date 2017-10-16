2459ff4e9ea8661c20336eb6990c7afa90d8768e
tangshengbo <867850520tangbo@sian.com>
2017-10-16 22:30:05 +0800

    add  memento patten code

---
+++ myproject-example/src/main/java/com/tangshengbo/design/memento/game/RoleStateCaretaker.java
@@ -0,0 +1,17 @@
+package com.tangshengbo.design.memento.game;
+
+/**
+ * Created by TangShengBo on 2017-10-16.
+ */
+public class RoleStateCaretaker {
+
+    private RoleStateMemento roleStateMemento;
+
+    public RoleStateMemento getRoleStateMemento() {
+        return roleStateMemento;
+    }
+
+    public void setRoleStateMemento(RoleStateMemento roleStateMemento) {
+        this.roleStateMemento = roleStateMemento;
+    }
+}
