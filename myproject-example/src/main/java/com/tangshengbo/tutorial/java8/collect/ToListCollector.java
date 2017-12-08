43995c1b178a90c46357b27b9b88aac16b20c4f4
Tangshengbo <867850520tangbo@sina.com>
2017-12-08 18:44:32 +0800

    add stream code

---
+++ myproject-example/src/main/java/com/tangshengbo/tutorial/java8/collect/ToListCollector.java
@@ -0,0 +1,45 @@
+package com.tangshengbo.tutorial.java8.collect;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collector.Characteristics.CONCURRENT;
+import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+
+/**
+ * Created by Tangshengbo on 2017/12/8.
+ */
+public class ToListCollector <T> implements Collector<T, List<T>, List<T>> {
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return List::add;
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return i -> i;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH, CONCURRENT));
+    }
+}
