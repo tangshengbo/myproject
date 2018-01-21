f3d5b26d7be9733f9ec3ed5779f3097b5e068fed
tangshengbo <867850520tangbo@sian.com>
2018-01-21 21:57:51 +0800

    add http code

--- myproject-example/src/main/java/com/tangshengbo/tutorial/http/RestTemplateTest.java
+++ myproject-example/src/main/java/com/tangshengbo/tutorial/http/RestTemplateTest.java
@@ -1,15 +1,22 @@
 package com.tangshengbo.tutorial.http;
 
-import org.apache.commons.lang.time.FastDateFormat;
+import com.tangshengbo.util.baofoo.util.ZipUtil;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static com.tangshengbo.util.baofoo.util.ZipUtil.decompress;
 
 /**
  * Created by Tangshengbo on 2018/1/19.
@@ -37,9 +44,7 @@ public class RestTemplateTest {
         params.put("age", "11");
         logger.info("{}", restTemplate.getForObject(url, String.class, params));
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, "唐声波", 11);
-        logger.info("{}", responseEntity.getStatusCode());
-        logger.info("{}", FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").format(responseEntity.getHeaders().getDate()));
-        logger.info("{}", responseEntity.getBody());
+        printLog(responseEntity);
     }
 
     @Test
@@ -52,7 +57,55 @@ public class RestTemplateTest {
         url = "http://localhost:8085/portal/account/list";
         ResponseEntity<String> responseEntity = null;
         responseEntity = restTemplate.postForEntity(url, "", String.class);
+        printLog(responseEntity);
+    }
 
+    @Test
+    public void testExchange() {
+        url = "http://localhost:8085/portal/account/search/{id}";
+        HttpHeaders header = new HttpHeaders();
+//        header.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        header.setContentType(MediaType.APPLICATION_JSON);
+//        JSONObject params = new JSONObject();
+//        params.put("id", "10");
+        HttpEntity<String> httpEntity = new HttpEntity<>(header);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class, 10);
+        printLog(responseEntity);
+    }
 
+    @Test
+    public void testDownFile() {
+//        url = "http://localhost:8085/portal/account/download-file/{fileName}";
+        url = "http://localhost:8085/portal/account/download-file-rest/{fileName}";
+        HttpHeaders header = new HttpHeaders();
+//        header.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        HttpEntity<String> httpEntity = new HttpEntity<>(header);
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity, byte[].class, "kk");
+        logger.info("{}", responseEntity.getStatusCode());
+        logger.info("{}", responseEntity.getHeaders());
+        byte[] body = responseEntity.getBody();
+        logger.info("{}", body);
+        List<String> stringList = ZipUtil.decompress(new ByteArrayInputStream(body));
+        printLog(stringList);
+    }
+
+    @Test
+    public void testDecompress() {
+        try {
+            List<String> stringList = decompress(new FileInputStream("E:/writer.zip"));
+            printLog(stringList);
+        } catch (FileNotFoundException e) {
+            logger.error("{}", ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    private void printLog(ResponseEntity<String> responseEntity) {
+        logger.info("{}", responseEntity.getStatusCode());
+        logger.info("{}", responseEntity.getHeaders());
+        logger.info("{}", responseEntity.getBody());
+    }
+
+    private void printLog(List<String> stringList) {
+        stringList.forEach(s -> logger.info("{}", s));
     }
 }
---
+++ myproject-example/src/main/java/com/tangshengbo/util/baofoo/util/ZipUtil.java
@@ -0,0 +1,191 @@
+package com.tangshengbo.util.baofoo.util;
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.*;
+
+/**
+ * Created by TangShengBo on 2018/1/21.
+ */
+public class ZipUtil {
+
+    static final int BUFFER = 8192;
+
+    public static void compress(String srcPath, String dstPath) throws IOException {
+        File srcFile = new File(srcPath);
+        File dstFile = new File(dstPath);
+        if (!srcFile.exists()) {
+            throw new FileNotFoundException(srcPath + "不存在！");
+        }
+
+        FileOutputStream out = null;
+        ZipOutputStream zipOut = null;
+        try {
+            out = new FileOutputStream(dstFile);
+            CheckedOutputStream cos = new CheckedOutputStream(out, new CRC32());
+            zipOut = new ZipOutputStream(cos);
+            String baseDir = "";
+            compress(srcFile, zipOut, baseDir);
+        } finally {
+            if (null != zipOut) {
+                zipOut.close();
+                out = null;
+            }
+
+            if (null != out) {
+                out.close();
+            }
+        }
+    }
+
+    private static void compress(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+        if (file.isDirectory()) {
+            compressDirectory(file, zipOut, baseDir);
+        } else {
+            compressFile(file, zipOut, baseDir);
+        }
+    }
+
+    /**
+     * 压缩一个目录
+     */
+    private static void compressDirectory(File dir, ZipOutputStream zipOut, String baseDir) throws IOException {
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            compress(files[i], zipOut, baseDir + dir.getName() + "/");
+        }
+    }
+
+    /**
+     * 压缩一个文件
+     */
+    private static void compressFile(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+        if (!file.exists()) {
+            return;
+        }
+
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(file));
+            ZipEntry entry = new ZipEntry(baseDir + file.getName());
+            zipOut.putNextEntry(entry);
+            int count;
+            byte data[] = new byte[BUFFER];
+            while ((count = bis.read(data, 0, BUFFER)) != -1) {
+                zipOut.write(data, 0, count);
+            }
+
+        } finally {
+            if (null != bis) {
+                bis.close();
+            }
+        }
+    }
+
+    //    将Zip文件加压缩出来，包含所有文件和文件夹到目标目录：
+    public static void decompress(String zipFile, String dstPath) throws IOException {
+        File pathFile = new File(dstPath);
+        if (!pathFile.exists()) {
+            pathFile.mkdirs();
+        }
+        ZipFile zip = new ZipFile(zipFile);
+        for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            String zipEntryName = entry.getName();
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = zip.getInputStream(entry);
+                String outPath = (dstPath + "/" + zipEntryName).replaceAll("\\*", "/");
+                //判断路径是否存在,不存在则创建文件路径
+                File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                //判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压
+                if (new File(outPath).isDirectory()) {
+                    continue;
+                }
+
+                out = new FileOutputStream(outPath);
+                byte[] buf1 = new byte[1024];
+                int len;
+                while ((len = in.read(buf1)) > 0) {
+                    out.write(buf1, 0, len);
+                }
+            } finally {
+                if (null != in) {
+                    in.close();
+                }
+
+                if (null != out) {
+                    out.close();
+                }
+            }
+        }
+    }
+
+    public static void decompress(File srcFile, File dstFile) {
+        ZipArchiveInputStream is = null;
+        try {
+
+            is = new ZipArchiveInputStream(new BufferedInputStream(new FileInputStream(srcFile)));
+            ZipArchiveEntry entry = null;
+            while ((entry = is.getNextZipEntry()) != null) {
+                if (entry.isDirectory()) {
+                    File directory = new File(dstFile, entry.getName());
+                    directory.mkdirs();
+                } else {
+                    OutputStream os = null;
+                    try {
+                        os = new BufferedOutputStream(new FileOutputStream(new File(dstFile, entry.getName())));
+                        IOUtils.copy(is, os);
+                    } finally {
+                        IOUtils.closeQuietly(os);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+    }
+
+    public static List<String> decompress(InputStream is) {
+        ZipArchiveInputStream zis;
+        zis = new ZipArchiveInputStream(IOUtils.buffer(is));
+        try {
+            if (Objects.nonNull(zis.getNextZipEntry())) {
+//                OutputStream os = new FileOutputStream("D:/" + archiveEntry.getName());
+//                IOUtils.copy(zis, os);
+                return IOUtils.readLines(new InputStreamReader(zis));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String targetFolderPath = "E:/kk.txt";
+        String rawZipFilePath = "E:/kk.zip";
+        String newZipFilePath = "E:/kk.zip";
+
+        //将Zip文件解压缩到目标目录
+//        ZipUtil.decompress(rawZipFilePath , targetFolderPath);
+
+        //将目标目录的文件压缩成Zip文件
+        ZipUtil.compress(targetFolderPath, newZipFilePath);
+
+    }
+}
