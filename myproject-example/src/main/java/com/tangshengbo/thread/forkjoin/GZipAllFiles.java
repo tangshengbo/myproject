239bdc35cf21bee5569029e4b3635791b315a864
Tangshengbo <867850520tangbo@sina.com>
2018-02-08 19:19:42 +0800

    add net code

--- myproject-example/src/main/java/com/tangshengbo/thread/forkjoin/GZipAllFiles.java
+++ myproject-example/src/main/java/com/tangshengbo/thread/forkjoin/GZipAllFiles.java
@@ -3,6 +3,8 @@ package com.tangshengbo.thread.forkjoin;
 import com.tangshengbo.datetime.DateUtils;
 import org.apache.commons.lang.time.FastDateFormat;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.concurrent.ExecutorService;
@@ -10,12 +12,16 @@ import java.util.concurrent.Executors;
 
 public class GZipAllFiles {
 
-    public final static int THREAD_COUNT = 4;
+    private static final Logger logger = LoggerFactory.getLogger(GZipAllFiles.class);
+
+    private final static int THREAD_COUNT = 4;
 
     @Test
-    public void testGZipFile() {
+    public void testGZipFile() throws Exception {
         ExecutorService pool = Executors.newFixedThreadPool(THREAD_COUNT);
         File rootFile = new File("E:/cas");
+        logger.info("{}", rootFile.toPath().getRoot());
+        System.out.println();
         String lastModifiedDate = FastDateFormat.getInstance(DateUtils.DEFAULT_DATE_FORMAT).format(rootFile.lastModified());
         System.out.println(lastModifiedDate);
         if (rootFile.exists()) {
