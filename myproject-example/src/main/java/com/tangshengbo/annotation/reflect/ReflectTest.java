3446a6a64edbf858ec8970043cac60331c7f2188
Tangshengbo <867850520tangbo@sina.com>
2019-01-15 20:46:00 +0800

    add reflect field convert code

--- myproject-example/src/main/java/com/tangshengbo/annotation/reflect/ReflectTest.java
+++ myproject-example/src/main/java/com/tangshengbo/annotation/reflect/ReflectTest.java
@@ -1,7 +1,12 @@
 package com.tangshengbo.annotation.reflect;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.tangshengbo.json.Account;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -39,7 +44,7 @@ public class ReflectTest {
             String path = this.getClass().getResource(DB_CONFIG_FILE).getPath();
 //            is = new FileInputStream(path);
 //            is = ReflectTest.class.getResourceAsStream("/folder/db.properties");
-            is =  ClassLoader.getSystemClassLoader().getResourceAsStream("folder/db.properties");
+            is = ClassLoader.getSystemClassLoader().getResourceAsStream("folder/db.properties");
             System.out.println(path);
             System.out.println(is);
 
@@ -66,5 +71,27 @@ public class ReflectTest {
                 break;
         }
         return server;
+    }
+
+    /**
+     * 1.用于获取结果集的映射关系
+     */
+    private String getResultsStr(Class origin) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("@Results({");
+        for (Field field : origin.getDeclaredFields()) {
+            String property = field.getName();
+            //映射关系：对象属性(驼峰)->数据库字段(下划线)
+            String column = new PropertyNamingStrategy.SnakeCaseStrategy().translate(field.getName())
+                    .toUpperCase();
+            stringBuilder.append(String.format("@Result(property = %s, column = %s)", property, column) + "\n");
+        }
+        stringBuilder.append("})");
+        return stringBuilder.toString();
+    }
+
+    @Test
+    public void testReflectResult() {
+        System.out.println(getResultsStr(Account.class));
     }
 }
